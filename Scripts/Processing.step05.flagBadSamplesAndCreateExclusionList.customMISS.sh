cd3967dd904c01b701e5d5c1c12b978652247676
Eric Yu <eyu8@beluga1.int.ets1.calculquebec.ca>
2019-12-02 16:10:11 -0500

    Added custom miss geno threshold

--- Scripts/Processing.step05.flagBadSamplesAndCreateExclusionList.customMISS.sh
+++ Scripts/Processing.step05.flagBadSamplesAndCreateExclusionList.customMISS.sh
@@ -6,11 +6,11 @@
 vcf=$1
 out=${2:-$(basename $vcf).sample2fractionNoCALLs}
 out2=$(basename $vcf).50PercentShitSamplesToExclude
-out3=$(basename $vcf).10PercentShitSamplesToExclude
+out3=$(basename $vcf).$3PercentShitSamplesToExclude
 if [[ ! -s $vcf ]]; then echo "WTF COWBOY YOUR INPUT FILE (NAME=$vcf) IS EMPTY OR DOES NOT EXIST"; exit 42; fi
 
 awk -F"\t" 'BEGIN{FS=OFS="\t"}{if ($0~/#/) {if ($1=="#CHROM") {for (i=10; i<=NF; i++) samples[i]=$i}; next}; total++; for (i=10; i<=NF; i++) if ($i~/^\.\/\./) c[samples[i]]++; }END{for (i in c) print i,c[i]/total}' $vcf |sort -k2n > $out
 
 
 cat $out | awk '{ if ($2>0.5) {print $1}}'  > $out2
-cat $out | awk -v cut=$3 '{ if ($2>0.1) {print $1}}'  > $out3
+cat $out | awk -v cut=$3 '{ if ($2>cut/100) {print $1}}'  > $out3
