b2d03f1e72cf88b6c7258c1e416a9d4ee1f9d2b0
Eric Yu <eyu8@beluga1.int.ets1.calculquebec.ca>
2020-07-20 18:46:46 -0400

    Added dp to step14 custom

--- Scripts/Processing.step14.SETIDprep.custom.sh
+++ Scripts/Processing.step14.SETIDprep.custom.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#Prepare SETID file for SKAT
+
 ##Set DIR in your seg analysis folder
 
 
-read DIR cohort DP <<< $@
+read DIR cohort <<< $@
 types="CADD Encode Funct LOF NS All"
 
-
-for type in $types; do
-  cat $DIR/$cohort/$DP/PD.$cohort.$DP.final.output.clean.OnlyRare$type.txt | awk -v cohort=$cohort -v DP=$DP -v type=$type 'BEGIN{FS=OFS="\t"}{if(NR>1) print $26"_"cohort""DP"_Rare"type,$6,$26,cohort"_"DP}' >> $DIR/Combined.setprep
+for DP in 30x 50x; do
+    for type in $types; do
+        cat $DIR/$cohort/$DP/PD.$cohort.$DP.final.output.clean.OnlyRare$type.txt | awk -v cohort=$cohort -v DP=$DP -v type=$type 'BEGIN{FS=OFS="\t"}{if(NR>1) print $26"_"cohort""DP"_Rare"type,$6,$26,cohort"_"DP}' >> $DIR/Combined.setprep
+    done
 done
 
 #$cohort.$DP.Rare$type.setprep
